51b6e8a90194452bed656fea845d5b39d63b14df
kh813 <10515511+kh813@users.noreply.github.com>
2026-06-18 08:32:39 +0900

    Add unit tests to verify GUI-less boot in test environment (v0.1.23)

---
+++ test/eliterm_application_test.exs
@@ -0,0 +1,22 @@
+defmodule ElitermApplicationTest do
+  use ExUnit.Case
+
+  test "in test environment, GUI, SleepWatcher, and WindowWatcher processes are bypassed" do
+    # Verify the configurations are disabled in test environment
+    refute Application.get_env(:eliterm, :start_gui)
+    refute Application.get_env(:eliterm, :start_sleep_watcher)
+    refute Application.get_env(:eliterm, :check_dependencies)
+
+    # Verify that these GUI-related processes are not running
+    assert Process.whereis(ElitermWindow) == nil
+    assert Process.whereis(Eliterm.WindowWatcher) == nil
+    assert Process.whereis(Eliterm.SleepWatcher) == nil
+  end
+
+  test "core processes are running in test environment" do
+    assert Process.whereis(Eliterm.Scheduler) != nil
+    assert Process.whereis(Eliterm.ClusterSupervisor) != nil
+    assert Process.whereis(Eliterm.PubSub) != nil
+    assert Process.whereis(ElitermWeb.Endpoint) != nil
+  end
+end
